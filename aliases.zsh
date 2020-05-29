13f62c441d97783c3b12ce5df8e52327fcebeae9
edalzell <erin@thedalzells.org>
2020-05-28 18:04:26 -0700

    customize aliases

--- aliases.zsh
+++ aliases.zsh
@@ -3,20 +3,21 @@ alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias reloadshell="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
-alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
-alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
 alias sites="cd $HOME/Sites"
-alias lara="sites && cd laravel/"
 
 # Laravel
-alias a="php artisan"
+alias art="php artisan"
 alias fresh="php artisan migrate:fresh --seed"
 alias seed="php artisan db:seed"
+
+
+# Statamic
+alias plz="php please"
 
 # PHP
 alias cfresh="rm -rf vendor/ composer.lock && composer i"
