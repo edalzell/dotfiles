7f9aba8980b57c59a1bedec09c9a3f11976fb554
edalzell <erin@thedalzells.org>
2020-05-30 11:11:32 -0700

    proper global composer packages and misc

--- fresh.sh
+++ fresh.sh
@@ -4,7 +4,7 @@ echo "Setting up your Mac..."
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /bin/bash -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # Update Homebrew recipes
@@ -15,14 +15,14 @@ brew tap homebrew/bundle
 brew bundle
 
 # Set default MySQL root password and auth type.
-#mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
+mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # Install PHP extensions with PECL
 pecl install xdebug
 
 # Install global Composer packages
 /usr/local/bin/composer global require hirak/prestissimo
-/usr/local/bin/composer global laravel/installer laravel/valet statamic/cli
+/usr/local/bin/composer global require laravel/installer statamic/cli laravel/valet
 
 # Install Laravel Valet
 $HOME/.composer/vendor/bin/valet install
@@ -31,8 +31,9 @@ $HOME/.composer/vendor/bin/valet install
 # This is a default directory for macOS user accounts but doesn't comes pre-installed
 mkdir $HOME/Sites
 
+
 # Clone Github repositories
-./clone.sh
+#./clone.sh
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
